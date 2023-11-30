eb8ebb08ff35bdeff77fb6f3cdc7c8a0d4b50020
Alex Quatro <dp050280sov@gmail.com>
2023-11-30 17:03:05 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -59,25 +59,6 @@
             }
         }));
     }
-    function formQuantity() {
-        document.addEventListener("click", (function(e) {
-            let targetElement = e.target;
-            if (targetElement.closest("[data-quantity-plus]") || targetElement.closest("[data-quantity-minus]")) {
-                const valueElement = targetElement.closest("[data-quantity]").querySelector("[data-quantity-value]");
-                let value = parseInt(valueElement.value);
-                if (targetElement.hasAttribute("data-quantity-plus")) {
-                    value++;
-                    if (+valueElement.dataset.quantityMax && +valueElement.dataset.quantityMax < value) value = valueElement.dataset.quantityMax;
-                } else {
-                    --value;
-                    if (+valueElement.dataset.quantityMin) {
-                        if (+valueElement.dataset.quantityMin > value) value = valueElement.dataset.quantityMin;
-                    } else if (value < 1) value = 1;
-                }
-                targetElement.closest("[data-quantity]").querySelector("[data-quantity-value]").value = value;
-            }
-        }));
-    }
     function ssr_window_esm_isObject(obj) {
         return obj !== null && typeof obj === "object" && "constructor" in obj && obj.constructor === Object;
     }
@@ -3990,5 +3971,4 @@
     window["FLS"] = true;
     isWebp();
     menuInit();
-    formQuantity();
 })();
